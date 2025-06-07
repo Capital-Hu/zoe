78b7b689fa8eb95c11b3b8c11877cb5362da9567
椰子瓜皮 <9281998+coconut-peel@user.noreply.gitee.com>
2025-06-07 20:19:28 +0800

    1.2 接入chatgpt或者deepseek

---
+++ src/main/java/com/zoe/Zoe_ChatLLM.java
@@ -0,0 +1,12 @@
+package com.zoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Zoe_ChatLLM {
+    public static void main(String[] args) {
+        SpringApplication.run(Zoe_ChatLLM.class, args);
+    }
+}
